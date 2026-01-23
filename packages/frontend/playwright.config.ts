a3ed331d61633ddb231e6b08de45a43ac8f97045
oxedom <oxedom@gmail.com>
2026-01-23 23:04:52 +0200

    fix: config

--- packages/frontend/playwright.config.ts
+++ packages/frontend/playwright.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 1,
+  workers: 1,
   reporter: 'html',
-
   use: {
     trace: 'on-first-retry',
   },
@@ -27,9 +26,5 @@ export default defineConfig({
     },
   ],
 
-  webServer: {
-    command: 'pnpm dev -- --host 127.0.0.1 --port 5173',
-    url: 'http://127.0.0.1:5173',
-    reuseExistingServer: !process.env.CI,
-  },
+
 });
